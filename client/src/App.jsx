728f3aeff2a1ce442f2bf7bd1888039aa99ef1fa
JD <jaysondorilag1@gmail.com>
2024-10-06 18:13:14 +0800

    Shadcn UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,22 @@
-import { Button } from "./components/ui/button"
-import { ThemeProvider } from "./components/theme-provider"
-import { ModeToggle } from "./components/mode-toggle"
+import { ThemeProvider } from "@/components/theme-provider";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "@/pages/auth/Login";
+import Navbar from "./components/Navbar";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
-
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-     <ModeToggle/>
-    <Button>Hello</Button>
-  </ThemeProvider>
-  )
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<LandingPage/>} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/LandingPage.jsx
@@ -0,0 +1,15 @@
+import FeatureSection from "@/components/FeatureSection";
+import FooterSection from "@/components/FooterSection";
+import HeroFormSignUpForm from "@/components/HeroFormSignUpForm";
+import HeroSection from "@/components/HeroSection";
+import QandASection from "@/components/QandASection";
+
+export default function LandingPage() {
+  return <>
+    <HeroSection/>
+    <FeatureSection/>
+    <HeroFormSignUpForm/>
+    <QandASection/>
+    <FooterSection/>
+  </>;
+}
