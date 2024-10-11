57e5ba9fb080184837d6e5536bbb4e9dbf0ae8fd
JD <jaysondorilag1@gmail.com>
2024-10-11 20:49:02 +0800

    Refactor CSS imports in LandingPage and SignUp components

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,5 +1,4 @@
 import FeatureSection from "@/components/FeatureSection";
-import HeroFormSignUpForm from "@/components/HeroFormSignUpForm";
 import HeroSection from "@/components/HeroSection";
 
 export default function LandingPage() {
